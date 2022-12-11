014b78a5d1bb835957a1e63f012287d0bd76dc5b
Eric Prud'hommeaux <eric+github@w3.org>
2022-12-11 12:11:51 +0100

    + filez

---
+++ index.js
@@ -0,0 +1,23 @@
+debugger
+
+let ismdwn = 0
+rpanrResize.addEventListener('mousedown', mD)
+
+function mD(event) {
+  ismdwn = 1
+  document.body.addEventListener('mousemove', mV)
+  document.body.addEventListener('mouseup', end)
+}
+
+function mV(event) {
+  if (ismdwn === 1) {
+    shexPanel.style.flexBasis = event.clientX + "px"
+  } else {
+    end()
+  }
+}
+const end = (e) => {
+  ismdwn = 0
+  document.body.removeEventListener('mouseup', end)
+  rpanrResize.removeEventListener('mousemove', mV)
+}
