6407ee7aea20012ef77bd9c8b1f2fb73c75e3d89
Eric Prud'hommeaux <eric+github@w3.org>
2022-12-12 08:59:04 +0100

    ~ split out ShExToShacl.js

---
+++ ShExToShacl.js
@@ -0,0 +1,208 @@
+class ShExToShacl {
+  constructor (indent, out, prefixes, base) {
+    this.indentation = indent;
+    this.out = out;
+    this.prefixes = prefixes;
+    this.base = base;
+  }
+
+  convert (shexj) {
+    const lead = '';
+    this.renderPrefixes(lead, this.prefixes);
+    this.out.write(`\n`);
+
+    if (shexj.shapes) {
+      const _ShExToShacl = this;
+      shexj.shapes.forEach(decl => {
+        if (decl.shapeExpr.type === 'Shape') {
+          _ShExToShacl.out.write(`${lead}${_ShExToShacl.iri(decl.id)}`);
+          _ShExToShacl.renderShapeExpression(lead, decl.shapeExpr);
+          _ShExToShacl.out.write(`${lead}.\n\n`);
+        } else {
+          _ShExToShacl.out.write(`${lead}# ${decl.id} is not a simple Shape\n`);
+        }
+      });
+    } else {
+      this.out.write(`${lead}# no shapes declared in ShExC\n`);
+    }
+  }
+
+  renderPrefixes (lead, prefixes) {
+    for (let [prefix, ns] of Object.entries(prefixes)) {
+      this.out.write(`${lead}PREFIX ${prefix}: <${ns}>\n`);
+    }
+  }
+
+  renderShapeExpression (lead, shapeExpr) {
+    switch (shapeExpr.type) {
+    case 'Shape':
+      this.renderShape(lead, shapeExpr);
+      break;
+    case 'NodeConstraint':
+      this.renderNodeConstraint(lead, shapeExpr);
+      break;
+    default:
+      this.out.write(`${lead}# unknown ShapeExpression type ${shapeExpr.type}\n`);
+    }
+  }
+
+  renderNodeConstraint (lead, nc) {
+    if ('nodeKind' in nc)
+      this.out.write(`${lead}${this.iri(Ns_shacl + "nodeKind")} ${this.iri(Ns_shacl + nc.nodeKind.toUpperCase())} ;\n`);
+    if ('datatype' in nc)
+      this.out.write(`${lead}${this.iri(Ns_shacl + "datatype")} ${this.iri(nc.datatype)} ;\n`);
+    if ('values' in nc) {
+      const values = nc.values.map(v => {
+        if (typeof v === 'string') return this.iri(v);
+        const langOrDt = 'language' in v
+              ? `@${v.language}`
+              : 'datatype' in v
+              ? `^^${this.iri(v.datatype)}`
+              : '';
+        return `"${v.value}"${langOrDt}`;
+      });
+      this.out.write(`${lead}${this.iri(Ns_shacl + "in")} (${values.join(' ')}) ;\n`);
+    }
+    if ('pattern' in nc) {
+      this.out.write(`${lead}${this.iri(Ns_shacl + "pattern")} "${nc.pattern}" ;\n`);
+      if ('flags' in nc) {
+        this.out.write(`${lead}${this.iri(Ns_shacl + "flags")} "${nc.flags}" ;\n`);
+      }
+    }
+    const x2cl = {
+      length: 'length',
+      minlength: 'minLength',
+      maxlength: 'maxLength',
+      mininclusive: 'minInclusive',
+      maxinclusive: 'maxInclusive',
+      minexclusive: 'minExclusive',
+      maxexclusive: 'maxExclusive',
+    };
+    for (let x in x2cl) {
+      if (x in nc) {
+        this.out.write(`${lead}${this.iri(Ns_shacl + x2cl[x])} "${nc[x]}" ;\n`);
+      }
+    }
+    if ('totaldigits' in nc) {
+      this.out.write(`${lead}${this.iri(Ns_shacl + "pattern")} "[0-9]{0,${nc.totaldigits}}\." ;\n`);
+    }
+    if ('fractiondigits' in nc) {
+      this.out.write(`${lead}${this.iri(Ns_shacl + "pattern")} "\.[0-9]{0,${nc.totaldigits}}" ;\n`);
+    }
+  }
+
+  renderShape (lead, sh) {
+    lead = this.indent(lead);
+    this.out.write(` a ${this.iri(Ns_shacl + "NodeShape")} ;\n`);
+    if (sh.closed) {
+      this.out.write(`${lead}${this.iri(Ns_shacl + "closed")} true;\n`);
+    }
+    const valueExpr = sh.expression;
+    if (!valueExpr) {
+      this.out.write(`${lead}# empty shape\n`);
+    } else {
+      this.renderTripleExpression(lead, valueExpr);
+    }
+    lead = this.outdent(lead);
+  }
+
+  renderTripleExpression (lead, tripleExpr) {
+    switch (tripleExpr.type) {
+    case 'TripleConstraint':
+      this.renderTripleConstraint(lead, tripleExpr);
+      break;
+    case 'EachOf':
+      const _ShExToShacl = this;
+      tripleExpr.expressions.forEach((conjunct, ord) => {
+        if (conjunct.type === 'TripleConstraint') {
+          _ShExToShacl.renderTripleConstraint(lead, conjunct);
+        } else {
+          _ShExToShacl.out.write(`${lead}# EachOf[${ord}] is not a TripleConstraint\n`);
+        }
+      });
+      break;
+    default:
+      this.out.write(`${lead}# unknown TripleExpression type: ${tripleExpr.type}\n`);
+    }
+  }
+
+  renderTripleConstraint (lead, tc) {
+    this.out.write(`${lead}${this.iri(Ns_shacl + "property")} [\n`);
+    lead = this.indent(lead);
+    this.out.write(`${lead}${this.iri(Ns_shacl + "path")} ${this.iri(tc.predicate)} ;\n`);
+
+    if ('min' in tc) {
+      if (tc.min !== 0)
+        this.out.write(`${lead}${this.iri(Ns_shacl + "minCount")} ${tc.min} ;\n`);
+    } else {
+      this.out.write(`${lead}${this.iri(Ns_shacl + "minCount")} 1 ;\n`);
+    }
+
+    if ('max' in tc) {
+      if (tc.max !== -1)
+        this.out.write(`${lead}${this.iri(Ns_shacl + "maxCount")} ${tc.max} ;\n`);
+    } else {
+      this.out.write(`${lead}${this.iri(Ns_shacl + "maxCount")} 1 ;\n`);
+    }
+
+    if ('valueExpr' in tc) {
+      // renderShapeExpression(lead, tc.valueExpr);
+      const valueExpr = tc.valueExpr;
+      if (typeof valueExpr === 'string') {
+        this.out.write(`${lead}${this.iri(Ns_shacl + "node")} ${this.iri(valueExpr)} ;\n`);
+      } else {
+        switch (valueExpr.type) {
+        case 'Shape':
+          this.out.write(`${lead}${this.iri(Ns_shacl + "node")} `);
+          this.renderShape(lead, valueExpr);
+          break;
+        case 'NodeConstraint':
+          this.renderNodeConstraint(lead, valueExpr);
+          break;
+        case 'ShapeOr':
+        case 'ShapeAnd':
+          const typeIri = valueExpr.type === 'ShapeOr'
+            ? Ns_shacl + "OR"
+            : Ns_shacl + "AND";
+          this.out.write(`${lead}${this.iri(typeIri)} (\n`);
+          lead = this.indent(lead);
+          const _ShExToShacl = this;
+          valueExpr.shapeExprs.forEach((junct, ord) => {
+            if (typeof junct === 'string') {
+              _ShExToShacl.out.write(`${lead}${_ShExToShacl.iri(junct)}\n`);
+            } else {
+              _ShExToShacl.out.write(`${lead}[\n`);
+              _ShExToShacl.renderShapeExpression(_ShExToShacl.indent(lead), junct);
+              _ShExToShacl.out.write(`${lead}]\n`);
+            }
+          });
+          lead = this.outdent(lead);
+          this.out.write(`${lead}) ;\n`);
+          break;
+        default:
+          this.out.write(`${lead}# unknown ShapeExpression type ${valueExpr.type}\n`);
+        }
+      }
+    }
+
+    lead = this.outdent(lead);
+    this.out.write(`${lead}] ;\n`);
+  }
+
+  iri (i) {
+    for (let [prefix, ns] of Object.entries(this.prefixes)) {
+      if (i.startsWith(ns)) {
+        return prefix + ':' + i.substr(ns.length);
+      }
+    }
+    return `<${i}>`;
+  }
+
+  // Indentation
+  indent (lead) {
+    return lead + this.indentation;
+  }
+  outdent (lead) {
+    return lead.substr(0, lead.length - this.indentation.length);
+  }
+}
--- index.js
+++ index.js
@@ -27,36 +27,33 @@ const yashe = YASHE(document.getElementById('shexc'), {
   //Options
 });
 
-document.getElementById("l2r").addEventListener('click', evt => {
-  copyL2R('  ');
-  return false;
-});
+document.getElementById("l2r").addEventListener('click', copyL2R);
+document.getElementById("r2l").addEventListener('click', copyR2L);
+
+
 document.addEventListener('keydown', evt => {
-  if (evt.ctrlKey && evt.altKey && evt.code === 'ArrowRight') {
-    copyL2R('  ');
-    return false;
+  if (evt.ctrlKey && evt.altKey) {
+    if (evt.code === 'ArrowRight')
+      return copyL2R();
+    if (evt.code === 'ArrowLeft')
+      return copyR2L();
   }
   return true;
 });
 
-document.getElementById("r2l").addEventListener('click', evt => {
-  copyR2L('  ');
-  return false;
-});
-document.addEventListener('keydown', evt => {
-  if (evt.ctrlKey && evt.altKey && evt.code === 'ArrowLeft') {
-    copyR2L('  ');
-    return false;
+function copyL2R () {
+  class TextAreaWriter {
+    constructor (elt) {
+      this.elt = elt;
+    }
+    write (text, encoding) {
+      this.elt.value += text;
+    }
+    end () { }
   }
-  return true;
-});
 
-function copyR2L (indent) {
-  alert("not implemented");
-}
-
-function copyL2R (indent) {
-  const lead = '';
+  shacl.value = ``;
+  const out = new TextAreaWriter(shacl);
   const url = "" + window.location;
   const shexc = yashe.getValue();
   const shexParser = ShExWebApp.Parser.construct(url, null, {index:true});
@@ -64,186 +61,25 @@ function copyL2R (indent) {
   const prefixes = shexj._prefixes;
   const base = shexj._base;
   prefixes['shacl'] = Ns_shacl;
-
-  shacl.value = ``;
-  renderPrefixes(lead, prefixes);
-  shacl.value += `\n`;
-
-  if (shexj.shapes) {
-    shexj.shapes.forEach(decl => {
-      if (decl.shapeExpr.type === 'Shape') {
-        shacl.value += `${lead}${iri(decl.id)}`;
-        renderShapeExpression(lead, decl.shapeExpr);
-        shacl.value += `${lead}.\n\n`;
-      } else {
-        shacl.value += `${lead}# ${decl.id} is not a simple Shape\n`;
-      }
-    });
-  } else {
-    shacl.value += `${lead}# no shapes declared in ShExC\n`;
-  }
-
-  function renderPrefixes (lead, prefixes) {
-    for (let [prefix, ns] of Object.entries(prefixes)) {
-      shacl.value += `${lead}PREFIX ${prefix}: <${ns}>\n`;
-    }
-  }
-
-  function renderShapeExpression (lead, shapeExpr) {
-    switch (shapeExpr.type) {
-    case 'Shape': renderShape(lead, shapeExpr); break;
-    case 'NodeConstraint': renderNodeConstraint(lead, shapeExpr); break;
-    default: shacl.value += `${lead}# unknown ShapeExpression type ${shapeExpr.type}\n`
-    }
-  }
-
-  function renderNodeConstraint (lead, nc) {
-    if ('nodeKind' in nc)
-      shacl.value += `${lead}${iri(Ns_shacl + "nodeKind")} ${iri(Ns_shacl + nc.nodeKind.toUpperCase())} ;\n`;
-    if ('datatype' in nc)
-      shacl.value += `${lead}${iri(Ns_shacl + "datatype")} ${iri(nc.datatype)} ;\n`;
-    if ('values' in nc) {
-      const values = nc.values.map(v => {
-        if (typeof v === 'string') return iri(v);
-        const langOrDt = 'language' in v
-              ? `@${v.language}`
-              : 'datatype' in v
-              ? `^^${iri(v.datatype)}`
-              : '';
-        return `"${v.value}"${langOrDt}`;
-      });
-      shacl.value += `${lead}${iri(Ns_shacl + "in")} (${values.join(' ')}) ;\n`;
-    }
-    if ('pattern' in nc) {
-      shacl.value += `${lead}${iri(Ns_shacl + "pattern")} "${nc.pattern}" ;\n`;
-      if ('flags' in nc) {
-        shacl.value += `${lead}${iri(Ns_shacl + "flags")} "${nc.flags}" ;\n`;
-      }
-    }
-    const x2cl = {
-      length: 'length',
-      minlength: 'minLength',
-      maxlength: 'maxLength',
-      mininclusive: 'minInclusive',
-      maxinclusive: 'maxInclusive',
-      minexclusive: 'minExclusive',
-      maxexclusive: 'maxExclusive',
-    };
-    for (let x in x2cl) {
-      if (x in nc) {
-        shacl.value += `${lead}${iri(Ns_shacl + x2cl[x])} "${nc[x]}" ;\n`;
-      }
-    }
-    if ('totaldigits' in nc) {
-      shacl.value += `${lead}${iri(Ns_shacl + "pattern")} "[0-9]{0,${nc.totaldigits}}\." ;\n`;
-    }
-    if ('fractiondigits' in nc) {
-      shacl.value += `${lead}${iri(Ns_shacl + "pattern")} "\.[0-9]{0,${nc.totaldigits}}" ;\n`;
-    }
-  }
-
-  function renderShape (lead, sh) {
-    lead = ind(lead);
-    shacl.value += ` a ${iri(Ns_shacl + "NodeShape")} ;\n`;
-    if (sh.closed) {
-    shacl.value += `${lead}${iri(Ns_shacl + "closed")} true;\n`;
-    }
-    const valueExpr = sh.expression;
-    if (!valueExpr) {
-      shacl.value += `${lead}# empty shape\n`;
-    } else {
-      renderTripleExpression(lead, valueExpr);
-    }
-    lead = out(lead);
-  }
-
-  function renderTripleExpression (lead, tripleExpr) {
-    switch (tripleExpr.type) {
-    case 'TripleConstraint': renderTripleConstraint(lead, tripleExpr); break;
-    case 'EachOf':
-      tripleExpr.expressions.forEach((conjunct, ord) => {
-        if (conjunct.type === 'TripleConstraint') {
-          renderTripleConstraint(lead, conjunct);
-        } else {
-          shacl.value += `${lead}# EachOf[${ord}] is not a TripleConstraint\n`;
-        }
-      });
-      break;
-    default: shacl.value += `${lead}# unknown TripleExpression type: ${tripleExpr.type}\n`;
-    }
-  }
-
-  function renderTripleConstraint (lead, tc) {
-    shacl.value += `${lead}${iri(Ns_shacl + "property")} [\n`;
-    lead = ind(lead);
-    shacl.value += `${lead}${iri(Ns_shacl + "path")} ${iri(tc.predicate)} ;\n`;
-
-    if ('min' in tc) {
-      if (tc.min !== 0)
-        shacl.value += `${lead}${iri(Ns_shacl + "minCount")} ${tc.min} ;\n`;
-    } else {
-      shacl.value += `${lead}${iri(Ns_shacl + "minCount")} 1 ;\n`;
-    }
-
-    if ('max' in tc) {
-      if (tc.max !== -1)
-        shacl.value += `${lead}${iri(Ns_shacl + "maxCount")} ${tc.max} ;\n`;
-    } else {
-      shacl.value += `${lead}${iri(Ns_shacl + "maxCount")} 1 ;\n`;
-    }
-
-    if ('valueExpr' in tc) {
-      // renderShapeExpression(lead, tc.valueExpr);
-      const valueExpr = tc.valueExpr;
-      if (typeof valueExpr === 'string') {
-          shacl.value += `${lead}${iri(Ns_shacl + "node")} ${iri(valueExpr)} ;\n`;
-      } else {
-        switch (valueExpr.type) {
-        case 'Shape':
-          shacl.value += `${lead}${iri(Ns_shacl + "node")} `;
-          renderShape(lead, valueExpr);
-          break;
-        case 'NodeConstraint':
-          renderNodeConstraint(lead, valueExpr);
-          break;
-        case 'ShapeOr':
-        case 'ShapeAnd':
-          const typeIri = valueExpr.type === 'ShapeOr'
-            ? Ns_shacl + "OR"
-            : Ns_shacl + "AND";
-          shacl.value += `${lead}${iri(typeIri)} (\n`;
-          lead = ind(lead);
-          valueExpr.shapeExprs.forEach((junct, ord) => {
-            if (typeof junct === 'string') {
-              shacl.value += `${lead}${iri(junct)}\n`
-            } else {
-              shacl.value += `${lead}[\n`
-              renderShapeExpression(ind(lead), junct);
-              shacl.value += `${lead}]\n`
-            }
-          });
-          lead = out(lead);
-          shacl.value += `${lead}) ;\n`;
-          break;
-        default: shacl.value += `${lead}# unknown ShapeExpression type ${valueExpr.type}\n`
-        }
-      }
-    }
-
-    lead = out(lead);
-    shacl.value += `${lead}] ;\n`;
-  }
-
-  function iri (i) {
-    for (let [prefix, ns] of Object.entries(prefixes)) {
-      if (i.startsWith(ns)) {
-        return prefix + ':' + i.substr(ns.length);
-      }
-    }
-    return `<${i}>`;
-  }
-
-  // Indentation
-  function ind (lead) { return lead + indent }
-  function out (lead) { return lead.substr(0, lead.length - indent.length); }
+  new ShExToShacl('  ', out, prefixes, base).convert(shexj);
+  return false;
 }
+
+function copyR2L () {
+  class CodeMirrorWriter {
+    constructor (editor) {
+      this.editor = editor;
+    }
+    write (text, encoding) {
+      // this.editor.editor.replaceRange(text, CodeMirror.Pos(this.editor.lastLine()));
+      this.editor.setValue(this.editor.getValue() + text); // inefficient
+    }
+    end () { }
+  }
+
+  // yashe.setValue('');
+  const out = new CodeMirrorWriter(yashe);
+  alert("not implemented");
+  return false;
+}
+
